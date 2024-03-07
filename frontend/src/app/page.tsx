6d1d074eb6932750010f891056edbf446f1c6b94
taxio <taxio.2029@gmail.com>
2024-03-07 21:30:40 +0900

    verify

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,6 +7,7 @@ import base64url from 'base64url';
 import { toBigInt } from "ethers";
 import { ECDSASigValue } from '@peculiar/asn1-ecc';
 import { AsnParser } from '@peculiar/asn1-schema';
+import * as ethers from 'ethers';
 
 async function parsePublicKeyBytes(publicKeyBytes: string): Promise<[BigInt, BigInt]> {
   const cap = {
@@ -51,6 +52,7 @@ function parseAuthSignature(authSignature: string): [BigInt, BigInt] {
 
 export default function Home() {
   const [username, setUsername] = useState("");
+  const [pubKey, setPubKey] = useState("");
 
   const register = async () => {
     const res = await webauthn.client.register(
@@ -69,19 +71,21 @@ export default function Home() {
       credentialId,
       pubKeyPos
     });
+    setPubKey(parsed.credential.publicKey);
   };
 
   const sign = async () => {
     // TODO: 署名させたいメッセージのハッシュ値 (をさらに Base64URL エンコードしたもの)
-    const challenge = window.crypto.randomUUID();
-    const res = await webauthn.client.authenticate(
+    const payload = ethers.keccak256(ethers.toUtf8Bytes("hello"));
+    const challenge = webauthn.utils.toBase64url(ethers.getBytes(payload)).replace(/=/g, '');
+    const authData = await webauthn.client.authenticate(
       [], // TODO: Contract に登録されている credentialId を入れる
       challenge, 
       {authenticatorType: 'auto'},
     );
-    console.debug('webauthn.client.authenticate():', res);
+    console.debug('webauthn.client.authenticate():', authData);
 
-    const parsed = webauthn.parsers.parseAuthentication(res);
+    const parsed = webauthn.parsers.parseAuthentication(authData);
     console.debug('webauthn.parsers.parseAuthentication():', parsed);
 
     const signature = parseAuthSignature(parsed.signature);
@@ -93,6 +97,30 @@ export default function Home() {
     // TODO:
     //  これらの結果が secp256r1 形式で署名検証できるかどうか確かめる
     //  それができれば、Contract 側で署名検証ロジックを再現できる
+    webauthn.server.verifySignature({
+      algorithm: 'ES256',
+      publicKey: pubKey,
+      authenticatorData: authData.authenticatorData,
+      clientData: authData.clientData,
+      signature: authData.signature,
+      verbose: true,
+    });
+    console.debug('verified');
+
+    const authDataBytes = new Uint8Array(webauthn.utils.parseBase64url(authData.authenticatorData));
+    const clientData = new TextDecoder().decode(webauthn.utils.parseBase64url(authData.clientData));
+    const challengePos = clientData.indexOf(challenge);
+    const challengePrefix = clientData.substring(0, challengePos);
+    const challengeSuffix = clientData.substring(challengePos + challenge.length);
+
+    const encodedSignature = ethers.AbiCoder.defaultAbiCoder().encode(
+      ["uint", "uint", "bytes", "string", "string"],
+      [signature[0], signature[1], authDataBytes, challengePrefix, challengeSuffix]
+    );
+    console.debug({
+      payload,
+      encodedSignature,
+    });
   };
 
 
