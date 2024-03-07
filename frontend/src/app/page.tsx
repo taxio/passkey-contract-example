b1f771c5282dd7d389b22412e41807fffc83f756
taxio <taxio.2029@gmail.com>
2024-03-07 16:21:11 +0900

    init frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Passkey on contract</h1>
+
+      {/* TODO: contract address */}
+
+      {/* TODO: Username */}
+
+      {/* TODO: register passkey */}
+
+      {/* TODO: sign and verify */}
+
+    </main>
+  );
+}
