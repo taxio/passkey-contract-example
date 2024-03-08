4ba0b572f34ef56703e6cc4e01b2e767c4dfdba2
taxio <taxio.2029@gmail.com>
2024-03-08 15:16:14 +0900

    contract deployer

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -11,10 +11,20 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  networks: {
+    polygonMumbai: {
+      chainId: 80001,
+      url: process.env.MUMBAI_RPC_URL!!,
+      accounts: [process.env.PRIVATE_KEY!!],
+    },
+  },
   gasReporter: {
     enabled: true,
     currency: "USD",
     gasPrice: 100,
+  },
+  etherscan: {
+    apiKey: process.env.SCAN_API_KEY!!,
   },
 };
 
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,22 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner] = await ethers.getSigners();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log("Owner:", owner.address);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const PasskeyAccount = await ethers.getContractFactory("PasskeyAccount");
+  const passkeyAccount = await PasskeyAccount.deploy();
+  console.log("deploy tx:", passkeyAccount.deploymentTransaction()?.hash);
+  console.log("deploy address:", await passkeyAccount.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
